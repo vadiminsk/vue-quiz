ed9a6ecfab81750979f6c713ae0f2798e4968f4c
Vadim <vadiminsk90@gmail.com>
2021-09-08 18:05:01 +0300

    Rendering the Questions

---
+++ src/components/AppResult.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">
+      Enter a short description here about the result.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppResult',
+};
+</script>
+
+<style></style>
